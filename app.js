c4c2ed57b2b5ed4154797be2cd5a58dd4d5d35a7
Angel Motta <angelmotta@gmail.com>
2021-05-25 12:05:21 -0500

    read and list implementation

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ yargs.command({
     command: 'list',
     describe: 'List notes',
     handler() {
-        console.log('Listing all notes');
+        notes.listNotes();
     }
 });
 
@@ -56,8 +56,15 @@ yargs.command({
 yargs.command({
     command: 'read',
     describe: 'Read a note',
-    handler() {
-        console.log('Reading a note');
+    builder: {
+        title: {
+            describe: 'Note to be listed',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        notes.readNote(argv.title);
     }
 });
 
--- notes.js
+++ notes.js
@@ -1,15 +1,10 @@
 const fs = require('fs');
 const chalk = require('chalk');
 
-const getNotes = () => {
-    return "Your notes...";
-}
-
 const addNote = (title, body) => {
     const notes = loadNotes();
-    const duplicateNotes = notes.filter((note) => note.title === title);
-
-    if (duplicateNotes.length === 0) {
+    const existDuplicate = notes.find((note) => note.title === title);
+    if (!existDuplicate) {
         notes.push({
             title: title,
             body: body
@@ -17,30 +12,6 @@ const addNote = (title, body) => {
         saveNotes(notes)
         console.log(chalk.green.inverse(`New note added!`));
     } else {
-        console.log(chalk.red.inverse(`Note title '${title}' already taken`));
-    }
-}
-
-const addNoteV2 = function(title, body) {
-    const notes = loadNotes();
-    isUnique = true;
-    for (var i = 0; i < notes.length; i++) {
-        if (notes[i].title == title) {
-            console.log(`Note title '${title}'. Please use another title`);
-            isUnique = false;
-            break;
-        }
-    }
-    if (isUnique) {
-        notes.push({
-            title: title,
-            body: body
-        });
-        // save to File
-        saveNotes(notes);
-        console.log(chalk.green.inverse(`New note added!`));
-    }
-    else {
         console.log(chalk.red.inverse(`Note title '${title}' already taken`));
     }
 }
@@ -77,13 +48,29 @@ const removeNote = (title) => {
     }
 }
 
-module.exports = {
-    getNotes: getNotes,
-    addNote: addNote,
-    removeNote: removeNote
+const listNotes = () => {
+    const notes = loadNotes();
+    console.log(chalk.inverse(`Your notes`));
+    notes.forEach(note => {
+        console.log(`Title: ${note.title}`);
+    });
 }
 
-// Sample export only function getNotes
-//module.exports = getNotes;
+const readNote = (title) => {
+    const notes = loadNotes();
+    const note = notes.find((note) => note.title === title);
+    if (note) {
+        console.log(chalk.inverse(`title: ${title}`));
+        console.log(note.body);
+    } else {
+        console.log(chalk.red.inverse(`Note not found`));
+    }
+}
 
+module.exports = {
+    addNote: addNote,
+    removeNote: removeNote,
+    listNotes: listNotes,
+    readNote: readNote
+}
 
